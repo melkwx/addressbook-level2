c5b5c2c4d11161760cbb43e5ee4705fedaa392a8
melkwx <melkwx@gmail.com>
2019-02-12 23:10:37 +0800

    Edited command prompt message

--- src/seedu/addressbook/ui/TextUi.java
+++ src/seedu/addressbook/ui/TextUi.java
@@ -79,7 +79,7 @@ public class TextUi {
      * @return command (full line) entered by the user
      */
     public String getUserCommand() {
-        out.print(LINE_PREFIX + "Enter command: ");
+        out.print(LINE_PREFIX + "What would you like to do? ");
         String fullInputLine = in.nextLine();
 
         // silently consume all ignored lines
